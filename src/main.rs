229f2307dae3ed8b03e8ec64a5d7f12b94070236
Aekkee <107569390+Aekkee@users.noreply.github.com>
2023-10-11 17:23:50 +0700

    change

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Write;
+use std::io::Read;
+
 fn main(){
-    
+    let v = vec!["test1.txt", "test2.txt", "test3.txt"];
+    doc_to_text_table(v, "output2.html")
 }
 
 #[test]
@@ -35,4 +41,82 @@ fn rank_documents(v: &Vec<String>) -> Vec<String> {
     let mut vsort = v.clone();
     vsort.sort_by(|x,y| y.matches("\n\n").count().cmp(&x.matches("\n\n").count()));
     vsort
+}
+
+#[allow(unused)]
+fn doc_to_table(v: Vec<&str>, output_file: &str) {
+    let mut docs: Vec<String> = Vec::new();
+    for i in v {
+        let mut data_file = File::open(i).unwrap();
+        let mut file_content = String::new();
+        data_file.read_to_string(&mut file_content).unwrap();
+        docs.push(file_content.replace("\r\n        \r\n", "\r\n\r\n"));
+    }
+
+    docs.sort_by(|x,y| y.matches("\r\n\r\n").count().cmp(&x.matches("\r\n\r\n").count()));
+    
+    let mut file = File::create(output_file).expect("Failed");
+    file.write(b"<style>");
+    file.write(b"\ntable, td, th {\n\tborder: 1px solid #000000;\n\tborder-collapse: collapse;\n}\n</style>\n");
+    file.write(b"<table>\n");
+    file.write(b"\t<tr>\n\t\t<th>text</th>\n\t\t<th>number of paragraph</th>\n\t</tr>");
+    for i in docs {
+        let mut count_line = 0;
+        let line = i
+            .chars()
+            .enumerate()
+            .filter(|(_, c)| *c == '\n')
+            .map(|(i,_)| i)
+            .collect::<Vec<_>>();
+        if !line.is_empty() {
+            for i in 0..line.len()-1 {
+                if line[i+1] - line[i] <= 2 {
+                    count_line += 1;
+                }
+            }
+        }
+        file.write(
+            format!(
+                "\t<tr>\n\t\t<td>{}</td>\n\t\t<td>{}</td>\n\t</tr>",
+                i,
+                count_line
+            )
+            .as_bytes(),
+        );
+    }
+    file.write(b"\n</table>");
+}
+
+#[allow(unused)]
+fn doc_to_text_table(v: Vec<&str>, output_file: &str) {
+    let mut docs: Vec<String> = Vec::new();
+    for i in v {
+        let mut data_file = File::open(i).unwrap();
+        let mut file_content = String::new();
+        data_file.read_to_string(&mut file_content).unwrap();
+        docs.push(file_content)
+    }
+
+    docs.sort_by(|x,y| y.split_ascii_whitespace().count().cmp(&x.split_ascii_whitespace().count()));
+
+    let mut file = File::create(output_file).expect("Failed");
+    file.write(b"<style>");
+    file.write(b"\ntable, td, th {\n\tborder: 1px solid #000000;\n\tborder-collapse: collapse;\n}\n</style>\n");
+    file.write(b"<table>\n");
+    file.write(b"\t<tr>\n\t\t<th>text</th>\n\t\t<th>number of paragraph</th>\n\t</tr>");
+    for i in docs {
+        let word = i
+            .split_ascii_whitespace()
+            .count();
+        println!("{:?}", i.split_ascii_whitespace().collect::<Vec<_>>());
+        file.write(
+            format!(
+                "\t<tr>\n\t\t<td>{}</td>\n\t\t<td>{}</td>\n\t</tr>",
+                i,
+                word
+            )
+            .as_bytes(),
+        );
+    }
+    file.write(b"\n</table>");
 }
